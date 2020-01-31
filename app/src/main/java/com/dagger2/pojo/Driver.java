8bb8ee900ff96d281192e44349a3193ec3bdd616
rishabhsaxena <rishabh2.saxena@paytm.com>
2020-01-31 11:32:55 +0530

    Improvements

--- app/src/main/java/com/dagger2/pojo/Driver.java
+++ app/src/main/java/com/dagger2/pojo/Driver.java
@@ -1,13 +1,5 @@
 package com.dagger2.pojo;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-@Singleton
 public class Driver {
-
-    @Inject
-    public Driver() {
-
-    }
+    // We don't own this.
 }
